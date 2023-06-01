641a8d97fe44ada7a3aa8598ada7a7dfd9b0b9f6
LorenzoVillegas <villegas.lorenzo@iisgalvanimi.edu.it>
2023-06-01 12:05:10 +0000

    secondo

--- facebook/src/app/app.component.ts
+++ facebook/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './post/post.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'facebook';
+
+  posts: Post[];
+
+  constructor(){
+    this.posts = [
+      new Post("mario rossi", "questo è il primo post di mario rossi"),
+      new Post("antonio bianchi", "questo è il post di antonio bianchi"),
+      new Post("mario rossi", "questo è il secondo post di Mario Rossi")
+    ]
+  }
+
+  addPost(autore: HTMLInputElement, testo: HTMLInputElement): boolean {
+    console.log(`Adding post autore: ${autore.value} and testo: ${testo.value}`);
+    this.posts.push(new Post(autore.value, testo.value));
+    //pulisco i campi della form
+    autore.value = '';
+    testo.value = '';
+    return false;
+  }
+
+  sortedPosts(): Post[] {
+    return this.posts.sort((a: Post, b: Post) => b.like - a.like);
+  }
+
 }
--- facebook/src/app/post/post.component.ts
+++ facebook/src/app/post/post.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card';
+  @Input() post: Post;
+
+  constructor() {
+    //this.article = new Article('Angular 2','http://angular.io',10);
+  }
+
+  voteUp(): Boolean {
+    this.post.add_like(); //Modificato qui 
+    return false;
+  }
+
+  ngOnInit() {}
 
 }
